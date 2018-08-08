199225ac93e5d8f3e01a39bc5b5a0f8dbc97c246
soojkim0306 <31287593+soojkim0306@users.noreply.github.com>
2018-08-07 22:32:05 -0700

    readded jwt

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,6 +4,7 @@ const Inbox = require('../models').Inbox;
 const Message = require('../models').Message;
 const Sequelize = require('sequelize');
 const config = require('../config/config');
+const jwt = require('jsonwebtoken');
 const Op = Sequelize.Op;
 const bcrypt = require('bcrypt');
 // const passport = require('passport');
